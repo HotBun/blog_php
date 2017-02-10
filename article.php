34eb1f9ca3003332561bf2674e93bfcf423b1fa5
Vasiliy Oleinik <goooneoooogle@gmail.com>
2017-02-11 00:15:59 +0200

    почти конец

--- article.php
+++ article.php
@@ -24,7 +24,10 @@ require "/includes/config.php";
    <?php include "/includes/header.php"; ?>
 
    <?php
-   $article = mysqli_query($connection, "SELECT * FROM `articles` WHERE `id` = 1");
+
+   //Открытие статьи 
+   $article = mysqli_query($connection, "SELECT * FROM `articles` WHERE `id` = 5");
+  //var_dump($_GET);
    //. (int) $_GET['id'])
 
    if ( mysqli_num_rows($article) <= 0 ){
@@ -52,7 +55,7 @@ require "/includes/config.php";
     <?php
   } else{
     $art = mysqli_fetch_assoc($article);
-    mysqli_query($connection, "UPDATE `articles` SET `views` = `views` + 1 WHERE `id` = " . (int) $art);
+    mysqli_query($connection, "UPDATE `articles` SET `views` = `views` + 1 WHERE `id` = " . (int) $art['id']);
     ?>
     <div id="content">
       <div class="container">
@@ -68,44 +71,106 @@ require "/includes/config.php";
                </div>
              </div>
            </div>
-         </section>
 
-         <div class="block">
-          <h3>Комментарии</h3>
-          <div class="block__content">
-            <div class="articles articles__vertical">
+           <div class="block">
+             <a href="#comment-add-form">Добавить комментарий</a>
+             <h3>Комментарии</h3>
+             <div class="block__content">
+              <div class="articles articles__vertical">
 
-              <?php 
-              $comments = mysqli_query($connection, "SELECT * FROM `comments` ORDER BY `id` DESC LIMIT 5");
-              ?>
+                <?php 
+                //Комментарии
+                $comments = mysqli_query($connection, "SELECT * FROM `comments` WHERE `articles_id` = ". (int) $art['id']. "ORDER BY `id` DESC" );
+                //. "ORDER BY `id` DESC"
+                if ( mysqli_num_rows($comments) <= 0 ){
+                  echo "Нет коммаентариев!<br/";
+                }
 
-              <?php
-              while( $comment = mysqli_fetch_assoc($comments)){
+                while( $comment = mysqli_fetch_assoc($comments)){
+                  ?>
+                  <article class="article">
+                    <div class="article__image" style="background-image: url(https://gravatar.com/avatar/<?php echo md5($comment['email']); ?>?s=80);"></div>
+                    <div class="article__info">
+                      <a href="article.php?id=<?php echo $comment['articles_id']; ?>"><?php echo $comment['autor'] ?></a>
+                      <div class="article__info__meta"></div>
+                      <div class="article__info__preview"><?php echo $comment['text'] ; ?></div>
+                    </div>
+                  </article>
+
+                  <?php
+                }
                 ?>
-                <article class="article">
-                  <div class="article__image" style="background-image: url(https://gravatar.com/avatar/<?php echo md5($comment['email']); ?>?s=80);"></div>
-                  <div class="article__info">
-                    <a href="article.php?id=<?php echo $comment['articles_id']; ?>"><?php echo $comment['autor'] ?></a>
-                    <div class="article__info__meta"></div>
-                    <div class="article__info__preview"><?php echo mb_substr(strip_tags($comment['text']), 0, 100, 'utf-8') . ' ...'; ?></div>
-                  </div>
-                </article>
 
-                <?php
-              }
-              ?>
-
+              </div>
             </div>
           </div>
-        </div>
-        
-        <section class="content__right col-md-4">
-          <?php include "includes/sidebar.php"; ?>
-        </section>
+
+          <div id="comment-add-form" class="block">
+           <h3>Добавить комментарий</h3>
+           <div class="block__content">
+             <form class="form" method="POST" action="/article.php?id=<?php echo $art['id']; ?> #comment-add-form" >
+
+               <?php   
+               if( isset($_POST['do_post']) ){
+                $errors = array();
+                if( $_POST['name'] == ''){
+                  $errors[] = 'Введите имя!';
+                }
+                if( $_POST['nickname'] == ''){
+                  $errors[] = 'Введите никнейм!';
+                }
+                if( $_POST['email'] == ''){
+                  $errors[] = 'Введите Email, его правда никто не увидит (кроме админа)!';
+                }
+                if( $_POST['text'] == ''){
+                  $errors[] = 'Вы забыли написать комментарий!';
+                }
+                if (empty($errors)){
+                  //добавить комментарий
+                   mysqli_query($connection, "INSERT INTO `comments` (`autor`, `email`, `nickname`, `text`, `pubdate`,`articles_id`) VALUES (mysql_real_escape_string('".$_POST['name']."'), mysql_real_escape_string('".$_POST['email']."'),mysql_real_escape_string('".$_POST['nickname']."'),mysql_real_escape_string('".$_POST['text']."'), NOW(),mysql_real_escape_string('".$art['id']."'))");
+
+
+
+                 echo '<span style="color:green; font-weight:700; margin-bottom:10px;">'. "Комментарий успешно добавлен" . '<hr/'. '</span>';
+               } else{
+                  //вывести ошибку
+                echo '<span style="color:red; font-weight:700; margin-bottom:10px;">'. $errors['0'] . '<hr/'. '</span>';
+              }
+
+            }
+            ;?>
+
+            <div class="form__group">
+             <div class="row">
+               <div class="col-md-4">
+                <input type="text" name="name" class="form__control" placeholder="Имя" value="<?php echo $_POST['name'];?>">
+              </div>
+              <div class="col-md-4">
+                <input type="text" name="nickname" class="form__control" placeholder="Никнейм" value="<?php echo $_POST['nickname'];?>">
+              </div>
+              <div class="col-md-4">
+                <input type="text" name="email" class="form__control" placeholder="Email  (не будет показан)" value="<?php echo $_POST['email'];?>">
+              </div>
+            </div>
+          </div>
+          <div class="form__group">
+            <textarea class="form__control" name="text" placeholder="Текст комментария..." ><?php echo $_POST['text'];?></textarea>
+          </div>
+          <div class="form__group">
+            <input type="submit" name="do_post" value="Добавить комментарий" class="form__control">
+          </div>
+        </form>
       </div>
     </div>
-  </div>
-  <?php
+
+  </section>
+  <section class="content__right col-md-4">
+    <?php include "includes/sidebar.php"; ?>
+  </section>
+</div>
+</div>
+</div>
+<?php
 }
 ?>
 
--- index.php
+++ index.php
@@ -79,97 +79,97 @@ require "includes/config.php";
               <h3>HTML/CSS [Последние записи]</h3>
               <div class="block__content">
                 <div class="articles articles__horizontal">
-                   <?php 
-                  $articles = mysqli_query($connection, "SELECT * FROM `articles` WHERE `categories_id` = 3 ORDER BY `id` DESC LIMIT 10");
-       
-                  while( $art = mysqli_fetch_assoc($articles)){
-                    ?>
-                    <article class="article">
-                      <div class="article__image" style="background-image: url(/static/img/<?php echo $art['images']; ?>);"></div>
-                      <div class="article__info">
-                        <a href="article.php?categories=<?php echo $art['id']; ?>"><?php echo $art['title']; ?></a>
-                        <div class="article__info__meta">
-
-                          <?php 
-                          $art_cat = false;
-                          foreach( $categories as $cat){
-                            if($cat['id'] == $art['categories_id']){
-                              $art_cat = $cat;
-                              break;
-                            }
-                          }
-                          ?>
-                          <small>
-                            Категория: <a href="/articles.php?categories=<?php echo $art_cat['id']; ?>">
-                            <?php echo $art_cat['title']; ?>
-                          </a></small>
-                        </div>
-
-
-                        <div class="article__info__preview"><?php echo mb_substr(strip_tags($art['text']), 0, 100, 'utf-8') . ' ...'; ?></div>
-                      </div>
-                    </article>
-
-                    <?php
-                  }
+                 <?php 
+                 $articles = mysqli_query($connection, "SELECT * FROM `articles` WHERE `categories_id` = 3 ORDER BY `id` DESC LIMIT 10");
+                 
+                 while( $art = mysqli_fetch_assoc($articles)){
                   ?>
-                </div>
+                  <article class="article">
+                    <div class="article__image" style="background-image: url(/static/img/<?php echo $art['images']; ?>);"></div>
+                    <div class="article__info">
+                      <a href="article.php?categories=<?php echo $art['id']; ?>"><?php echo $art['title']; ?></a>
+                      <div class="article__info__meta">
+
+                        <?php 
+                        $art_cat = false;
+                        foreach( $categories as $cat){
+                          if($cat['id'] == $art['categories_id']){
+                            $art_cat = $cat;
+                            break;
+                          }
+                        }
+                        ?>
+                        <small>
+                          Категория: <a href="/articles.php?categories=<?php echo $art_cat['id']; ?>">
+                          <?php echo $art_cat['title']; ?>
+                        </a></small>
+                      </div>
+
+
+                      <div class="article__info__preview"><?php echo mb_substr(strip_tags($art['text']), 0, 100, 'utf-8') . ' ...'; ?></div>
+                    </div>
+                  </article>
+
+                  <?php
+                }
+                ?>
               </div>
             </div>
+          </div>
 
-            <div class="block">
-              <a href="article.php?categories=5">Все записи</a>
-              <h3>SQL [Последние записи]</h3>
-              <div class="block__content">
-                <div class="articles articles__horizontal">
+          <div class="block">
+            <a href="article.php?categories=5">Все записи</a>
+            <h3>SQL [Последние записи]</h3>
+            <div class="block__content">
+              <div class="articles articles__horizontal">
 
-                  <?php 
-                  $articles = mysqli_query($connection, "SELECT * FROM `articles` WHERE `categories_id` = 5 ORDER BY `id` DESC LIMIT 10");
-       
-                  while( $art = mysqli_fetch_assoc($articles)){
-                    ?>
-                    <article class="article">
-                      <div class="article__image" style="background-image: url(/static/img/<?php echo $art['images']; ?>);"></div>
-                      <div class="article__info">
-                        <a href="article.php?categories=<?php echo $art['id']; ?>"><?php echo $art['title']; ?></a>
-                        <div class="article__info__meta">
-
-                          <?php 
-                          $art_cat = false;
-                          foreach( $categories as $cat){
-                            if($cat['id'] == $art['categories_id']){
-                              $art_cat = $cat;
-                              break;
-                            }
-                          }
-                          ?>
-                          <small>
-                            Категория: <a href="/articles.php?categories=<?php echo $art_cat['id']; ?>">
-                            <?php echo $art_cat['title']; ?>
-                          </a></small>
-                        </div>
-
-
-                        <div class="article__info__preview"><?php echo mb_substr(strip_tags($art['text']), 0, 100, 'utf-8') . ' ...'; ?></div>
-                      </div>
-                    </article>
-
-                    <?php
-                  }
+                <?php 
+                $articles = mysqli_query($connection, "SELECT * FROM `articles` WHERE `categories_id` = 5 ORDER BY `id` DESC LIMIT 10");
+                
+                while( $art = mysqli_fetch_assoc($articles)){
                   ?>
+                  <article class="article">
+                    <div class="article__image" style="background-image: url(/static/img/<?php echo $art['images']; ?>);"></div>
+                    <div class="article__info">
+                      <a href="article.php?categories=<?php echo $art['id']; ?>"><?php echo $art['title']; ?></a>
+                      <div class="article__info__meta">
 
-                </div>
+                        <?php 
+                        $art_cat = false;
+                        foreach( $categories as $cat){
+                          if($cat['id'] == $art['categories_id']){
+                            $art_cat = $cat;
+                            break;
+                          }
+                        }
+                        ?>
+                        <small>
+                          Категория: <a href="/articles.php?categories=<?php echo $art_cat['id']; ?>">
+                          <?php echo $art_cat['title']; ?>
+                        </a></small>
+                      </div>
+
+
+                      <div class="article__info__preview"><?php echo mb_substr(strip_tags($art['text']), 0, 100, 'utf-8') . ' ...'; ?></div>
+                    </div>
+                  </article>
+
+                  <?php
+                }
+                ?>
+
               </div>
             </div>
-          </section>
-          <section class="content__right col-md-4">
-           <?php include "/includes/sidebar.php"; ?>
-         </section>
-       </div>
+          </div>
+        </section>
+        <section class="content__right col-md-4">
+         <?php include "/includes/sidebar.php"; ?>
+       </section>
      </div>
    </div>
+ </div>
 
-  <?php include "/includes/footer.php"; ?>
+ <?php include "/includes/footer.php"; ?>
 
 </div>
 
